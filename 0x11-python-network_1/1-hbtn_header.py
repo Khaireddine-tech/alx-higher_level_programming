eeaa3ea2410c9f557bf711c0e147c77412a53e00
khaireddine arbouch <66341920+khaireddine-arbouch@users.noreply.github.com>
2024-05-10 23:05:48 +0300

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" module doc """
-import urllib.request
-import sys
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        print(response.info()["X-Request-Id"])
